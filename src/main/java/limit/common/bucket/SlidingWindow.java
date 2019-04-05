bb00715c15f80ab37feaa43332714a96b8e7f1f4
hellozepp <zhanglin04@daojia-inc.com>
2019-04-05 19:10:05 +0800

    修改注释

--- src/main/java/limit/common/bucket/SlidingWindow.java
+++ src/main/java/limit/common/bucket/SlidingWindow.java
@@ -5,11 +5,8 @@ import com.alibaba.csp.sentinel.EntryType;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.context.ContextUtil;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.google.common.util.concurrent.Monitor;
 import com.google.common.util.concurrent.RateLimiter;
 import limit.common.sentinel.ControlRole;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author: zhanglin
@@ -27,7 +24,6 @@ public class SlidingWindow {
      * The smallest count window chunk length in bytes
      */
     private static Long WINDOW_LENGTH = 1024L;
-    private ControlRole controlRole;
 
     /**
      * How many bytes will be sent or receive
@@ -39,24 +35,30 @@ public class SlidingWindow {
      */
     private long recentFinishTime = System.nanoTime();
 
+    //use the guava ControlRole to control sliding window
+    private ControlRole controlRole;
+
     /**
      * Default rate is 1024KB/s
      */
     private int maxRate = 1024;
+
     private static final int DEFAULT_MAX_RATE = 1024;
+
     /**
      * Time cost for sending WINDOW_LENGTH bytes in nanoseconds
      */
     private long timeCostPerChunk = (1000000000L * WINDOW_LENGTH) / (this.maxRate * KB);
-    //定义桶的大小 Bytes
+
+    //the package max size (Bytes)
     private static final int DEFAULT_PACKAGE_SIZE = 8192;
+
     private static final int DEFAULT_BUCKET_NUM = DEFAULT_MAX_RATE * KB / DEFAULT_PACKAGE_SIZE;
-    private AtomicInteger curBucketSize = new AtomicInteger();
-    //消费者 不论多少个线程，每秒最大的处理能力是1秒中执行10次
+
+    //use the sentinel ControlRole to control sliding window
     private RateLimiter producerRate;
+
     private int bucketNum;
-    //往桶里面放数据时，确认没有超过桶的最大的容量
-    private Monitor offerMonitor = new Monitor();
 
     public int getBucketNum() {
         return bucketNum;
@@ -78,7 +80,6 @@ public class SlidingWindow {
             throw new IllegalArgumentException("maxRate can not less than 0");
         }
         this.maxRate = maxRate;
-
         bucketNum = maxRate * KB / DEFAULT_PACKAGE_SIZE;
         System.out.println("[setMaxRate] bucketNum:" + bucketNum);
         if (bucketNum < 0) {
